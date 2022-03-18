887d1f951ff0b9fa4167b16128fc3a62f7289b01
pythonDON0161 <70659505+pythonDON0161@users.noreply.github.com>
2022-03-17 21:03:21 -0500

    Backend update

---
+++ backend/sendMail.js
@@ -0,0 +1,36 @@
+
+const nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'autofinancejamaica@gmail.com',
+      pass: 'E2M*u[Epfgv$GsfX'
+    }
+  });
+
+
+  transporter.verify((err, success) => {
+    err
+      ? console.log(err)
+      : console.log(`=== Server is ready to take messages: ${success} ===`);
+   });
+   
+
+   transporter.sendMail(mailOptions, function (err, data) {
+    if (err) {
+      console.log("Error " + err);
+    } else {
+      console.log("Email sent successfully");
+    }
+   });
+   
+
+  transporter.sendMail(mailOptions, function (err, data) {
+    if (err) {
+      console.log("Error " + err);
+    } else {
+      console.log("Email sent successfully");
+    }
+   });
+
--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,56 @@
 // server/index.js
 
 const express = require("express");
+const cors = require("cors");
+const nodemailer = require('nodemailer');
+
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-
+// middleware
+app.use(express.json());
+app.use(cors());
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
   console.log("it works!!")
 });
+
+
+app.get("/sendmail", (req, res) => {
+ 
+  var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'autofinancejamaica@gmail.com',
+      pass: 'E2M*u[Epfgv$GsfX'
+    }
+  });
+ 
+  let mailOptions = {
+    from: `${req.body.mailerState.email}`,
+    to: process.env.EMAIL,
+    subject: `Message from: ${req.body.mailerState.email}`,
+    text: `${req.body.mailerState.message}`,
+  };
+ 
+  transporter.sendMail(mailOptions, function (err, data) {
+    if (err) {
+      res.json({
+        status: "fail",
+      });
+    } else {
+      console.log("== Message Sent ==");
+      console.log("it works!!")
+      res.json({
+        status: "success",
+      });
+    }
+})
+})
+
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
