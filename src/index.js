39c79f456b277bc3594d52ea76ccdd05804c4cbf
pythonDON0161 <70659505+pythonDON0161@users.noreply.github.com>
2022-08-14 19:48:18 -0500

    removed basenameurl from github

--- src/index.js
+++ src/index.js
@@ -116,7 +116,7 @@ function App() {
       <StateMachineProvider>
         {process.env.NODE_ENV !== "production" && <DevTool />}
         <ChakraProvider theme={theme}>
-          <Router basename={process.env.PUBLIC_URL}>
+          <Router>
             <Route exact path="/" component={LandingPage} />
             <Route path="/home" component={Home} />
             <Route path="/loan-calculator" component={Calculator} />
