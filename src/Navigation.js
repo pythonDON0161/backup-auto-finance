c5d3f01c52deda9ffdcaca6d74ef4685506c103b
pythonDON0161 <70659505+pythonDON0161@users.noreply.github.com>
2022-10-10 00:52:24 -0500

    Changed design & check if module completed

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,5 +1,5 @@
 import {React, useState} from "react";
-import { withRouter, Link } from "react-router-dom";
+import { withRouter, Link,  useHistory } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import { useStateMachine } from "little-state-machine";
 import updateAction from "./updateAction";
@@ -11,23 +11,45 @@ import {
   ListItem,
   ListIcon,
   OrderedList,
-  UnorderedList,HStack
+  UnorderedList,HStack,Button
 } from "@chakra-ui/react";
 import { FeedbackFish } from "@feedback-fish/react";
 import Header from "./components/Header";
 import {FiMenu,FiHome,FiCalendar,FiUsers,FiDollarSign,FiBriefcase,FiUser,FiSettings
 } from 'react-icons/fi'
 import{AiOutlineBank} from 'react-icons/ai'
+import {ImArrowDown} from 'react-icons/im'
 import NavItem from './components/NavItem';
 import {GrDocumentUpload} from 'react-icons/gr'
+import { Warning } from "postcss";
 
+
+function subApp(){
+
+}
 
 
 const Navigation = (props) => {
+
   
   const { action, state } = useStateMachine(updateAction);
 
+  const [clicked, setClicked] = useState('hidden');
+
+  
+  const [clicked2, setClicked2] = useState('hidden');
+
+  const handleClick = () => {
+    state.data.ratio ?  history.push('/bank-selection') : setClicked('show');
+  };
+
+  const handleClick2 = () => {
+    state.data.ratio ? history.push('/upload-page'): setClicked2('show');
+  };
+
   const [navSize, changeNavSize] = useState("large");
+
+  const history = useHistory();
 
   //funciton to reset individual state variables
 
@@ -37,62 +59,89 @@ const Navigation = (props) => {
       
         <SimpleGrid columns={1} >
 
+             <Center> 
+                 <Heading textAlign="center">Road Map to Finding My Deal</Heading>
+              </Center>
+
           <Center>
-             <Text fontSize='lg' fontWeight="bold">
-             
-              </Text>
-
-              <p style={{fontWeight:700 }}>
-              We are Going to Ask You for some Information – in Order to Find You the Best Bank Deal.
-              <br/>
-              Please complete Step 1 (Personal Information) before moving onto Other Steps.
-              <br/>
-              Steps 2 (Co-Applicant) and Step 4 (Document Upload) are Optional
-
-
+            <Text> 
+            <p style={{ fontWeight:600,justifyContent: "center",
+                        alignItems: "center",textAlign: "center" }}>
+            We are Going to Ask You for some Information – in Order to Find You the Best Bank Deal.
+           Please complete Step 1 (Personal Information) before moving onto Other Steps.    <br/> 
+            Steps 2 (Co-Applicant) and Step 4 (Document Upload) are Optional.
               </p>
-
-       
-          
+            </Text>
+           
           </Center>
         
           </SimpleGrid>
 
-          <div className="coapp" style={{padding:0}}>
+      <div className="coapp" style={{padding:0}}>
 
-        <SimpleGrid spacing={4}>          
+        <SimpleGrid spacing={1}>          
+
             <Center> 
             <Link to="/pre-qualification"> 
-            <HStack> 
-               <Text  fontSize='3xl' fontWeight="bold"> Personal Information</Text>
-            </HStack>
+                <HStack> 
+                  <button className="nav-button">PERSONAL INFORMATION</button>
+                </HStack>
             </Link>
             </Center>
+
+          <Center  p={0}> <ImArrowDown  color="#e65323" size={50} /> </Center>
+            
             
            <Center> 
            <Link to="/co-app-disclaimer">
-          <HStack> 
-              <Text  fontSize='3xl' fontWeight="bold">Co-Applicant Information</Text>
-           </HStack>
+              <HStack> 
+                  <button className="nav-button">CO-APPLICANT INFORMATION</button>
+              </HStack>
            </Link>
            </Center>
+
+           <Center p={0}> <ImArrowDown color="#e65323" size={50} /> </Center>
            
-           <Link to="/additional-info"> 
-           <Center> 
-          <HStack> 
-              <Text fontSize='3xl' fontWeight="bold" > Bank Selection</Text>
-           </HStack>
+       
+           <Center p={0}> 
+           <Link > 
+              <HStack> 
+                  <button  onClick={ handleClick } className="nav-button">BANK SELECTION</button>
+              </HStack>
+              </Link>
            </Center>
-           </Link>
 
-           <Link to="/upload-page"> 
-           <Center> 
-          <HStack> 
-              <Text fontSize='3xl' fontWeight="bold"> Document Upload</Text>
-           </HStack>
+              <Center> 
+                  <Text style={{width:"70%",textAlign:"center",color:"red",fontWeight:600, border:"3px red solid"}} 
+                  className={clicked}> Please complete Personal Information Module !</Text>
+              </Center>
+         
+           <Center p={0}> <ImArrowDown p={0} color="#e65323" size={50} /> </Center>
+
+           
+           <Center p={0} > 
+            <Link > 
+              <HStack> 
+                <button onClick={ handleClick2 } className="nav-button">DOCUMENT UPLOAD</button>
+              </HStack>
+            </Link>
            </Center>
-           </Link>
 
+              <Center> 
+                  <Text style={{width:"70%",textAlign:"center",color:"red",fontWeight:600, border:"3px red solid"}} 
+                  className={clicked2}> Please complete Personal Information Module !</Text>
+                </Center>
+
+           <Center mt={5}> 
+              <Link to="/upload-page"> 
+                  <button  style={{backgroundColor:"green"}} className="nav-button">FINISH/SUBMIT MY APPLICATION</button>
+              </Link>
+           </Center>
+
+           <Spacer>
+
+           </Spacer>
+          
           </SimpleGrid>
 
 
