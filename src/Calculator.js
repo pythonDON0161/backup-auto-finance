d0d9c81cf13c5e2f8924e648d98753e9883d5b48
pythonDON0161 <70659505+pythonDON0161@users.noreply.github.com>
2022-10-14 19:49:48 -0500

    Changed circle color

--- src/Calculator.js
+++ src/Calculator.js
@@ -598,7 +598,7 @@ function Calculator(props) {
                       <SliderTrack>
                         <SliderFilledTrack />
                       </SliderTrack>
-                      <SliderThumb bg='yellow' />
+                      <SliderThumb bg='blue' />
                     </Slider>
                     {state.data.rate >= 0 && <h1>{rate}% Interest Rate</h1>}{" "}
                     {state.data.rate == null && (
@@ -619,7 +619,7 @@ function Calculator(props) {
                       <SliderTrack>
                         <SliderFilledTrack />
                       </SliderTrack>
-                      <SliderThumb bg='yellow' />
+                      <SliderThumb bg='blue' />
                     </Slider>
 
                    
@@ -642,7 +642,7 @@ function Calculator(props) {
                       <SliderTrack>
                         <SliderFilledTrack />
                       </SliderTrack>
-                      <SliderThumb bg='yellow' />
+                      <SliderThumb bg='blue' />
                     </Slider>
                     <Center>
                       <h1>Estimated Monthly Payment:&nbsp;</h1>
