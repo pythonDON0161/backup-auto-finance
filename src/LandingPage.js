4eed15fa8d6a4241926f474fd286bae8db9ac854
Dominic Summers <domz.summers@gmail.com>
2023-01-12 17:24:43 -0500

    Update LandingPage.js

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -28,7 +28,7 @@ function LandingPage() {
                     <a href="/">
                       <img
                         className="h-auto w-24"
-                        src="/raceCar.png"
+                        src="/LoanSelecta.png"
                         alt="Logo"
                       />
                     </a>
