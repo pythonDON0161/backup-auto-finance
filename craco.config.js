dca5a65d332e129b19a532e957354ed7144d23cc
Dominic Summers <domz.summers@gmail.com>
2023-03-08 12:47:35 -0500

    Add Buffer to Webpack plugins alongside Process

--- craco.config.js
+++ craco.config.js
@@ -14,6 +14,7 @@ module.exports = {
         add: [
             new webpack.ProvidePlugin({
                 process: 'process/browser.js',
+                Buffer: ["buffer", "Buffer"],
             })
         ]
     }
